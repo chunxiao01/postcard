de3894e51c7b09e79c2e92522d16bba0e5b499bc
chunxiao01 <12861187+chunxiao01@users.noreply.github.com>
2023-01-05 16:33:41 +0800

    chore:基本框架搭建.

---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<script setup></script>
+<template>
+  <div class="card">
+    <div class="happy-new-year">
+      <div class="happy-new-year-content">
+        <div class="happy-new-year-text">新年好！</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 100%;
+  height: 100vh;
+}
+.happy-new-year {
+  width: 100%;
+  height: 100%;
+}
+
+.happy-new-year-content {
+  width: 100%;
+  height: 100%;
+  background: #e60000;
+}
+
+.happy-new-year-text {
+  color: #ffd700;
+  font-size: 30px;
+}
+</style>
